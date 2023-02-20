be8104dbbd0e3099eee8918b4bf742e1191efec2
Borrdom <86350432+Borrdom@users.noreply.github.com>
2023-02-20 09:23:22 +0100

    swelling considerd

--- diffusionpy/Stefan_Maxwell_segmental.py
+++ diffusionpy/Stefan_Maxwell_segmental.py
@@ -2,7 +2,7 @@ import numpy as np
 import casadi as cs
 import time
 import copy
-
+import matplotlib.pyplot as plt
 def SolveODEs(x,f,x0,opts,Time):
     dae={"x":x,"t":Time,"ode":f}
     fint=cs.integrator("integrator","idas",dae,opts)
@@ -11,6 +11,13 @@ def SolveODEs(x,f,x0,opts,Time):
 
 def averaging(a):
     return (a[1:]+a[:-1])/2
+def diff(a):
+    #abar=averaging(a)
+    #b=4/3*(a[1:]-a[:-1])
+    #c=b[:1]-2/12*(abar[1:]-abar[:-1])
+    #c=b[1:]+1/2*a[2:]-1/2*a[:-2]
+    #return cs.horzcat(a[1]-abar[0],c)
+    return (a[1:]-a[:-1])
 
 def Diffusion_MS(t,L,Dvec,w0,w8,Mi,volatile,full_output=False,Gammai=None):
     """
@@ -25,28 +32,31 @@ def Diffusion_MS(t,L,Dvec,w0,w8,Mi,volatile,full_output=False,Gammai=None):
     w0:    array_like   Mass fractions at t=0               /-
     w8:    array_like   Mass fraction at t=infinity         /-
     Mi:    array_like   Molar mass of components nc         /g/mol
-    lngammai: array_like estimate for lngammai at t         /t
+    Gammai: array_like estimate for the TH-factor at t      /-
     Returns
     -------
     wt:    array_like   Matrix of mass fractions at t       /-
+
+    Full output
+    -------
+    wt:    array_like   Matrix of mass fractions at t       /-
+    wtz:    array_like  Matrix of mass fractions at t,z     /-
     """
-    #volatile=volatile.flatten()
-    #w0=w0.flatten()
-    #w8=w0.flatten()
-    #Mi=Mi.flatten()
-    #t=t.flatten()
-    #Dvec=Dvec.flatten()
-    #Dref=1E-14
+
+    #Dref=np.average(Dvec)
     #tau=(L**2)/Dref
     nc=len(w0)
-    D=D_Matrix(Dvec,nc)
     nz=20
+    dz=L/nz
+    D=D_Matrix(Dvec/dz**2,nc)
+    
     zvec=np.linspace(0,L,nz+1)
     nf=np.sum(volatile)
     nt=len(t)
     rho=1200
     refsegment=np.argmin(Mi)
-    #initial
+
+    #initial conditions
     rhoiinit=cs.DM.zeros((nc,nz+1))
     for z in range(nz+1):
         rhoiinit[:,z]=w0*rho
@@ -56,11 +66,7 @@ def Diffusion_MS(t,L,Dvec,w0,w8,Mi,volatile,full_output=False,Gammai=None):
     #decision variable vector
     rhov=cs.MX.sym("rhov",(nf,nz+1))
     Time=cs.MX.sym("Time")
-    #lngammai=np.zeros((nc,nt))
-    #lngammai=lngammai.T
-    #lngamma0i=lngammai[:,0]
-    #lngamma8i=lngammai[:,-1]
-    #lngammaiT=cs.MX.zeros(nc)
+    x=cs.reshape(rhov,(np.multiply(*rhov.shape),1))
     
     GammaiT=cs.MX.ones(nc)
     if Gammai is not None:
@@ -74,8 +80,6 @@ def Diffusion_MS(t,L,Dvec,w0,w8,Mi,volatile,full_output=False,Gammai=None):
     rhoi=cs.MX.ones((nc,nz+1))
     rhoi[np.where(volatile)[0],:]=rhov
     rhoi[np.where(~volatile)[0],:]=rhoiinit[np.where(~volatile)[0],:]
-    rhoi=cs.fmax(rhoi,1E-8)
-
 
     
     ji=cs.MX.zeros((nc,nz))
@@ -84,56 +88,63 @@ def Diffusion_MS(t,L,Dvec,w0,w8,Mi,volatile,full_output=False,Gammai=None):
     rhoibar=cs.MX.ones((nc,nz))
     drhoidt=cs.MX.ones((nc,nz+1))
     wi=cs.MX.zeros((nc,nz+1))
-    dz=L/nz
+    
     ri= Mi/Mi[refsegment]
 
     for i in range(nc):
         wi[i,:]=rhoi[i,:]/cs.sum1(rhoi)
-        dlnwi[i,:]= cs.diff(cs.log(wi[i,:]))*GammaiT[i]
+        dlnwi[i,:]=cs.diff(cs.log(wi[i,:]))*GammaiT[i]
         wibar[i,:]= averaging(wi[i,:])
         rhoibar[i,:]= averaging(rhoi[i,:])
-
-    
     for z in range(nz):
-        B=BIJ_Matrix(D,wibar[:,z],volatile)
+        B=BIJ_Matrix(D,wibar[:,z],volatile) 
         allflux=nc==nf
         Binv=cs.inv(B)
-        dmui=(dlnwi[:,z])/dz
-        di=rhoibar[:,z]*dmui/ri
+        dmui=(dlnwi[:,z])
+        #di=rhoibar[:,z]*dmui/ri # in rhoibar steckt die transformierte riesige Dichte drin. Der Volumenexpansionsfaktor korriegiert diesen
+        omega=rho/cs.sum1(rhoibar[:,z])
+        di=rho*wibar[:,z]*dmui/ri*omega
+        
         if not allflux:
-            ji[np.where(volatile)[0],z]=cs.sum1(di[np.where(volatile)[0]]*Binv)
+            ji[np.where(volatile)[0],z]=di[np.where(volatile)[0]].T@Binv
         else:
-            ji[:-1,z]=cs.sum1(di[:-1]*Binv)
-    
-    ji[-1,:]=-cs.sum1(ji[:-1,:]) if allflux else ji[-1,:]
+            ji[:-1,z]=di[:-1].T@Binv
+
     for i in range(nc):
         dji=cs.diff(cs.horzcat(0,ji[i,:]))
-        drhoidt[i,:]=cs.horzcat(dji/dz,0)
+        djib=cs.horzcat(dji,0) 
+        drhoidt[i,:]=djib
+    # need for boundary when swelling
+    # drhoidtmean=cs.sum2(drhoidt[:,:-1])/nz
+    # drhoidt[:,-1]=cs.sum1(drhoidtmean)*w8
+    #is this it
+
     drhovdt=drhoidt[np.where(volatile)[0],:]
     ode=cs.reshape(drhovdt,(np.multiply(*drhovdt.shape),1))
-    x=cs.reshape(rhov,(np.multiply(*rhov.shape),1))
+    
     xinit=cs.reshape(rhovinit,(np.multiply(*rhovinit.shape),1))
 
-    #np.cond(A) 10**16 
-    
 
-    opts = {"grid":t,"max_num_steps":1000,"regularity_check":True,"output_t0":True,"verbose":True}
     opts = {"grid":t,"regularity_check":True,"output_t0":True,"verbose":True}
     start=time.time_ns()
     x_sol=SolveODEs(x,ode,xinit,opts,Time)
     end=time.time_ns()
     print("------------- Diffusion modeling took "+str((end-start)/1E9)+" seconds ----------------")
     nt=len(t)
-    wt=np.ones((nc,nt))
-    wik=np.ones((nt,nc,nz+1))
-    rhoik=rhoiinit
+    wt=np.zeros((nc,nt))
+    wik=np.zeros((nt,nc,nz+1))
+    rhoik=np.zeros((nt,nc,nz+1))
+    rhok=np.zeros(nt)
     for k in range(nt):
         rhovk=cs.reshape(x_sol[:,k],(nf,nz+1))
-        rhoik[np.where(volatile)[0],:]=rhovk
-        for i in range(nc): 
-            wik[k,i,:]=rhoik[i,:]/cs.sum1(rhoik)
+        rhoik[k,:,:]=rhoiinit
+        rhoik[k,np.where(volatile)[0],:]=rhovk
+        for i in range(nc):
+            wik[k,i,:]=rhoik[k,i,:]/cs.sum1(rhoik[k,:,:]).full()
+        rhok[k]=cs.sum2(cs.sum1(rhoik[k,:,:-1])/nz).full()
         wt[:,k]=cs.sum2(wik[k,:,:-1]/nz).full().flatten()
-    return wt.T if not full_output else (wt.T,wik,zvec)
+    Lt=rhok/rho*L
+    return wt.T if not full_output else (wt.T,wik,zvec,Lt)
 
 
 def BIJ_Matrix(D,wi,volatile):
@@ -150,7 +161,7 @@ def BIJ_Matrix(D,wi,volatile):
         BII=Dii if not allflux else Dii+Din
         B[i,:]=BIJ
         B[i,i]=BII
-    #alpha=1/np.min(D)
+    #alpha=1/np.min(D) #implement an augmented transport matrix
     #B+alpha*wi@wi.T
 
     return B[np.where(volatile)[0],np.where(volatile)[0]] if not allflux else B[:-1,:-1]  #8E-13
@@ -211,30 +222,40 @@ def Diffusion1D(t,L0,Ds,ws0,ws8):
 
 if __name__=="__main__":
     nt=100
-    t=np.linspace(0,400,nt)*60
+    t=np.linspace(0,2450,nt)*60
     nc=3
     nd=(nc-1)*nc//2 
-    Dvec=np.asarray([1E-13,2.3E-13,1.7E-13])
-    Dvec=np.asarray([1E-10,2.3E-10,3E-14])
+    Dvec=np.asarray([4E-11,9E-11,1.7E-11])
+    #Dvec=np.asarray([1E-10,2.3E-10,3E-14])
     #np.fill_diagonal(D,np.ones(nc)*1E-30)
-    L=0.001
+    L=0.0002
     wi0=np.asarray([0.01,0.495,0.495])
-    wi8=np.asarray([0.9,0.005,0.095])
+    wi8=np.asarray([0.9,0.095,0.005])
     Mi=np.asarray([18.015,357.57,65000])
 
-    volatile=np.asarray([True,True,False])
-    wt,wtz,zvec=Diffusion_MS(t,L,Dvec,wi0,wi8,Mi,volatile,True)
+    volatile=np.asarray([True,True,True])
+    wt,wtz,zvec,Lt=Diffusion_MS(t,L,Dvec,wi0,wi8,Mi,volatile,True)
     import matplotlib.pyplot as plt
     fig,ax=plt.subplots()
     fig1,ax1=plt.subplots()
+    fig2,ax2=plt.subplots()
+    fig3,ax3=plt.subplots()
     ax.plot(t/60,wt[:,0])
     ax.plot(t/60,wt[:,1])
     ax.plot(t/60,wt[:,2])
-    #[ax1.plot(zvec,wtz[i*10,0,:]) for i,val in enumerate(t[::10])]
-    [ax1.plot(zvec,wtz[i*10,1,:]) for i,val in enumerate(t[::10])]
-    #[ax1.plot(zvec,wtz[i*10,2,:]) for i,val in enumerate(t[::10])]
+    [ax1.plot(zvec*1E6,wtz[i*10,0,:]) for i,val in enumerate(t[::10])]
+    [ax2.plot(zvec*1E6,wtz[i*10,1,:]) for i,val in enumerate(t[::10])]
+    [ax3.plot(zvec*1E6,wtz[i*10,2,:]) for i,val in enumerate(t[::10])]
     ax.set_xlabel("t/min")
     ax.set_ylabel("wi/-")
     ax.legend(["w1","w2","w3"])
+    ax1.set_xlabel("z/m")
+    ax1.set_ylabel("ww/-")
+    ax2.set_xlabel("z/m")
+    ax2.set_ylabel("wapi/-")
+    ax3.set_xlabel("z/m")
+    ax3.set_ylabel("wpol/-")
+    #ax1.legend(["w1","w2","w3"])
+
     plt.show()
 
