76a4687d3431969e185416aaa135e37c4321f1d3
Borrdom <86350432+Borrdom@users.noreply.github.com>
2023-03-13 17:08:22 +0100

    new surface concentration time-dependant 2

--- diffusionpy/xloil_functions.py
+++ diffusionpy/xloil_functions.py
@@ -8,8 +8,8 @@ import time
 
 @xlo.func
 def Diffusion_MS_xloil(t:xlo.Array(float,dims=1),L:float,Dvec:xlo.Array(float,dims=1),w0:xlo.Array(float,dims=1),w8:xlo.Array(float,dims=1),Mi:xlo.Array(float,dims=1),
-volatile:xlo.Array(bool,dims=1),full_output:bool,Gammai:xlo.Array(float,dims=2)=None,w0II:xlo.Array(float,dims=1)=None):   
-    return Diffusion_MS(t,L,Dvec,w0,w8,Mi,volatile,full_output,Gammai,w0II)
+volatile:xlo.Array(bool,dims=1),full_output:bool,Gammai:xlo.Array(float,dims=2)=None,swelling:bool=False,taui:xlo.Array(float,dims=1)=None):   
+    return Diffusion_MS(t,L,Dvec,w0,w8,Mi,volatile,full_output,Gammai,swelling,taui)
 
 @xlo.func
 def Diffusion1D_xloil(t:xlo.Array(float,dims=1),L0:float,Ds:float,ws0:float,ws8:float):
