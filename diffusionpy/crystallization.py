5877300b1fe7bdfdf3ac8885c9a036bdfd69cf8e
Borrdom <86350432+Borrdom@users.noreply.github.com>
2023-06-16 10:42:28 +0200

    implement crystallization

--- diffusionpy/crystallization.py
+++ diffusionpy/crystallization.py
@@ -4,7 +4,7 @@ from scipy.interpolate import interp1d
 from.PyCSAFT_nue import lngi,vpure
 from.Stefan_Maxwell_segmental import D_Matrix
 from scipy.integrate import solve_ivp
-
+import matplotlib.pyplot as plt
 
 
 crystallize=np.asarray([False,False,True])
@@ -16,22 +16,25 @@ TSL=np.asarray([429.47])
 cpSL=np.asarray([87.44])
 ww=np.asarray([0.27087,0.22302, 0.13792, 0.09208, 0.06118])
 dl=np.asarray([0,0.1 , 0.3 , 0.5 , 0.68])
-DAPI=np.asarray([0.59010330E-17])
-sigma=np.asarray([2.97730286E-02])
-kt=np.asarray([8.0778700E-12])
-g=np.asarray([3.92])
+DAPI=np.asarray([6.6E-17])
+sigma=np.asarray([1.98E-02])
+kt=np.asarray([5.1E-12])
+g=np.asarray([3.2])
 Mi=np.asarray([18.015,65000.,230.26])
 rho0i=np.asarray([997.,1180.,1320.])
 
 wv_fun=interp1d(dl,ww,bounds_error=False,fill_value=(0.27087,0.06118))
 
 nc=3
-wi0=np.asarray([0.01,0.495,0.495])
-wi8=np.asarray([0.1,0.4,0.4])
+wv0=0.0001
+dl0=0.68
+wi0=np.asarray([wv0,(1-wv0)*(1-dl0),(1-wv0)*dl0])
+wv8=wv_fun(dl0)
+wi8=np.asarray([wv8,(1-wv8)*(1-dl0),(1-wv8)*dl0])
 T=298.15
 p=1E5
 
-kij=D_Matrix(np.asarray([-0.156,-0.025,0]),nc)
+kij=D_Matrix(np.asarray([-0.156,0.00648,-0.0574]),nc)
 par={"mi":np.asarray([1.20469,2420.99, 8.105152]),
 "si": np.asarray([2.797059952,2.947, 2.939]),
 "ui" :np.asarray([353.95,205.27, 229.45]),
@@ -45,7 +48,7 @@ vpures=vpure(p,T,**par)
 
 par["vpure"]=vpures
 nt=300
-t=np.linspace(0,30000,nt)*60
+t=np.linspace(0,3000,nt)*60
 
 lngi_fun=lambda wi: lngi(T,wi,**par)
 
@@ -53,31 +56,33 @@ lngi_fun=lambda wi: lngi(T,wi,**par)
 
 
 def Bound(t,mobiles,immobiles,crystallize,wi0,wi8,rho0i,Mi,DAPI,sigma,kt,g,deltaHSL,TSL,cpSL,lngi_fun=None,wv_fun=None):
-    crystallizes=np.where(crystallize)
+    crystallizes=np.where(crystallize)[0]
     alpha0=0.01
-    r0=0.01
+    r0=0.1
     def dxdt(t,x):
         alpha,r=x[0],x[1]
-        dalphadt,dalphadr=Kris(alpha,r,mobiles,immobiles,crystallize,wi0,wi8,rho0i,Mi,DAPI,sigma,kt,g,deltaHSL,TSL,cpSL,lngi_fun,wv_fun)
+        dalphadt,dalphadr=Kris(t,alpha,r,mobiles,immobiles,crystallize,wi0,wi8,rho0i,Mi,DAPI,sigma,kt,g,deltaHSL,TSL,cpSL,lngi_fun,wv_fun)
         return np.hstack((dalphadt,dalphadr))
     x0=np.hstack((alpha0,r0))
     xsol=solve_ivp(dxdt,(t[0],t[-1]),x0,method="Radau",t_eval=t)["y"]
     alpha,r=xsol[0],xsol[1]
-    dl_la = (wi0[crystallizes]-alpha)/(1-alpha)
+    dl0=wi0[crystallizes]/np.sum(wi0[immobiles])
+    dl_la = (dl0-alpha)/(1-alpha)
     wvB=wv_fun(dl_la)
     wiB=np.zeros((wi0.shape[0],t.shape[0]))
     wiB[mobiles,:]=wvB
-    wiB[immobiles,:]=(1-np.sum(wiB[mobiles],axis=0))*np.expand_dims(wi0[immobiles]/np.sum(wi0[immobiles]),1)
+    wiB[crystallizes,:]=(1-wvB)*dl_la
+    wiB[immobiles[crystallizes!=immobiles],:]=(1-wvB)*(1-dl_la)
     return wiB,alpha,r
 
-def Kris(alpha,r,mobiles,immobiles,crystallize,wi0,wi8,rho0i,Mi,DAPI,sigma,kt,g,deltaHSL,TSL,cpSL,lngi_fun=None,wv_fun=None):
+def Kris(t,alpha,r,mobiles,immobiles,crystallize,wi0,wi8,rho0i,Mi,DAPI,sigma,kt,g,deltaHSL,TSL,cpSL,lngi_fun=None,wv_fun=None):
     crystallizes=np.where(crystallize)[0]
     R=8.31445
     NA=6.023E23
     kB=R/NA
     temp=298.15
     AR=100
-    M=Mi[crystallizes]
+    M=Mi[crystallizes]/1000.
     rho=rho0i[crystallizes]
     pre=rho*np.pi/(4*AR**2)
     scale_r=1E-6
@@ -90,14 +95,12 @@ def Kris(alpha,r,mobiles,immobiles,crystallize,wi0,wi8,rho0i,Mi,DAPI,sigma,kt,g,
 
     wv=wv_fun(dl_la) if callable(wv_fun) else wv_fun
     wi[mobiles]=wv
-    wi[crystallizes]=(1-np.sum(wv,axis=0))*dl_la
-    wi[immobiles[crystallizes!=immobiles]]=(1-np.sum(wv,axis=0))*(1-np.sum(dl_la,axis=0))*wi0[immobiles[crystallizes!=immobiles]]/np.sum(wi0[immobiles])
+    wi[crystallizes]=(1-wv)*dl_la
+    wi[immobiles[crystallizes!=immobiles]]=(1-wv)*(1-dl_la)
     lnaiSLE=-deltaHSL/(R*temp)*(1-temp/TSL)+cpSL/R*(TSL/temp-1-np.log(TSL/temp))
     lnai=lngi_fun(wi)+np.log(wi)
     dmu_sla=lnai[crystallizes]-lnaiSLE
-    print(dmu_sla)
-    # if alpha>0.5:
-    #     print(dl_la)
+    # plt.plot(t,np.exp(dmu_sla),"kx")
     rstar = ((2*sigma)/(C0*kB*temp*dmu_sla))
     deltaG=sigma**3*(16*np.pi)/(3*(C0*kB*temp*dmu_sla)**2)
     wv=wi[mobiles]
@@ -118,7 +121,10 @@ import matplotlib.pyplot as plt
 
 
 # plt.plot(t/60,alpha)
-plt.plot(t/60,wiB[0,:])
+# plt.plot(t/60,r)
+# plt.plot(t/60,wiB[0,:])
+# plt.plot(t/60,wiB[1,:])
+# plt.plot(t/60,wiB[2,:])
 plt.show()
 
 def Krisnewerbut(wi,alpha,r,ww_fun,mobiles,crystallize,wi0,wi8,rho0i,Mi,DAPI,sigma,kt,g,deltaHSL,TSL,cpSL,lngi_fun=None):
